79b3be9ca2135884909fe8a02f9f75e8abd5b274
Karolina Zalasińska <karolinaa.zalasinska@gmail.com>
2023-11-09 22:55:41 +0100

    GlobalExceptionHandler

--- src/main/java/exceptions/GlobalExceptionHandler.java
+++ src/main/java/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package exceptions;
 
 import exceptions.copies.CopyNotAvailableException;
+import exceptions.reviews.EmptyDescriptionException;
+import exceptions.reviews.InvalidRatingException;
+import exceptions.reviews.ReviewAlreadyExistsException;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -15,6 +18,21 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleObjectNotFoundInRepository(ObjectNotFoundInRepositoryException e) {
         Long id = e.getId();
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Object not found with ID " + id);
+    }
+
+    @ExceptionHandler(ReviewAlreadyExistsException.class)
+    public ResponseEntity<Object> handleReviewAlreadyExistsException(ReviewAlreadyExistsException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Review already exists: " + e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidRatingException.class)
+    public ResponseEntity<Object> handleInvalidRatingException(InvalidRatingException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid rating: " + e.getMessage());
+    }
+
+    @ExceptionHandler(EmptyDescriptionException.class)
+    public ResponseEntity<Object> handleEmptyDescriptionException(EmptyDescriptionException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Empty description: " + e.getMessage());
     }
 
     @ExceptionHandler(SomeCustomException.class)
